0d56735d5b319eaa715b0bc7cafe452885eebd3a
Nikola Vlaskalin <97924033+grtsrb@users.noreply.github.com>
2022-01-22 11:14:04 +0100

    Update reciever.cpp

--- reciever/reciever.cpp
+++ reciever/reciever.cpp
@@ -4,40 +4,45 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
-
-
+ 
+ 
 RCSwitch mySwitch;
 #define BUF_LEN 80
 typedef enum { F, T } boolean;
-
-void gameGuesser(){
-    int num, lastNum, value, triesNum = 0;
-    boolean true = F;
+ 
+void gameGuesser(int file){
+    int num, lastNum=0, value, triesNum = 0;
+    int istrue = T;
     char gameRun;
     srand(time(NULL));
     num = rand() % 100 + 1;
-
+ 
     while(1){
-        if(true == T){
+        if(istrue == T){
           printf("We are honored. New game has started! \n");
-          true = F;
+          istrue = F;
           triesNum = 0;
           num = rand() % 100 + 1;
         }
-
+ 
         if (mySwitch.available()){
           value = mySwitch.getReceivedValue();
-
+ 
           if(value == 0) printf("Unknonw encoding!\n");
           else{
             if (num != lastNum){
               printf("Recieved %i\n", value);
               triesNum++;
-
               if (value == num){
                 printf("Congratulations, you have guessed the correct number! Number of tries: %d\n", triesNum);
-                write(file_desc, &value, 1);
-                true = T;
+                write(file, &value, 1);
+                istrue = T;
+		 printf("Do you want to play again? Y for true, F for false. \n");
+          	scanf("%c", &gameRun);
+          	if (gameRun == 'F'){
+            	   printf("Thanks for playing! Reciever out!\n");
+            	   return;
+          	}
               } else if (value < num) printf("Too bad, number you are looking for is bigger!\n");
                 else printf("Too bad, number you are looking for is smaller!\n");
             }
@@ -47,40 +52,35 @@ void gameGuesser(){
           mySwitch.resetAvailable();
         }
           usleep(100);
-          printf("Do you want to play again? Y for true, F for false. \n");
-          scanf("%c", gameRun);
-          if (gameRun == 'F'){
-            printf("Thanks for playing! Reciever out!\n");
-            return;
-          }
+ 
     }
-
+ 
 }
-
+ 
 int main(int argc, char *argv[]) {
-
+ 
      int PIN = 2;
-     int file_desc= open("/dev/gpio_driver", O_RDWR); 
-
+     int file_desc = open("/dev/gpio_driver", O_RDWR); 
+ 
     if (file_desc < 0){
       printf("Error, could not open file.");
       return -1;
     }
-
+ 
     if(wiringPiSetup() == -1) {
        printf("wiringPiSetup failed, exiting...");
        return -2;
      }
-
+ 
      int pulseLength = 0;
      if (argv[1] != NULL) pulseLength = atoi(argv[1]);
-
+ 
      mySwitch = RCSwitch();
      if (pulseLength != 0) mySwitch.setPulseLength(pulseLength);
      mySwitch.enableReceive(PIN);
-     
-     gameGuesser();
+ 
+     gameGuesser(file_desc);
      close(file_desc);
-
+ 
      exit(0);
 }
